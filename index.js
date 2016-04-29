8137749c634f1f303639faf7f58e4e7d0d78a00d
lrlna <shestak.irina@gmail.com>
2016-04-29 11:24:29 -0700

    :cool: filter empty fields :tada:

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-//#! /usr/bin/env node
+var csv = require('csv-parse')
+var fs = require('fs')
+var path = require('path')
+var through2 = require('through2')
+var Trello = require('node-trello')
 
-var csv      = require("csv-parse")
-var fs       = require("fs")
-var path     = require("path")
-var through2 = requite("through2")
-var Trello   = require("node-trello")
-
-var file = process.argv[1]
+var file = process.argv[2]
 
 var stream = csv({columns: true})
 var readable = fs.createReadStream(file)
   .pipe(stream)
   .pipe(through2.obj(transform))
   .on('data', function (data) {
-    process.stdout.write(data + "\n")
+    console.log(data + "\n")
   })
   .on('error', function (err) {
-    process.stdout.write("There is an error reading the csv file")
-    process.stdout.write(err)
-    process.exit(1)
+    console.log("There is an error reading the csv file")
+    console.log(err)
+    console.log(1)
+  })
+  .on('end', function() {
+    console.log("end of csv")
   })
 
 function transform (chunk, enc, callback) {
-  var tags = []
+  var row = []
   Object.keys(chunk).forEach(function (key) {
-    process.stdout.write(chunk[key])
+    !!chunk[key] ? row.push(chunk[key]) : continue 
   })
 
   // create item from chunk's keys; skip if chunk[key] is empty
-  var item = {
-  }
-
-  j
+  //var item = {
+  //}
 }
 
 function createCard (item) {
