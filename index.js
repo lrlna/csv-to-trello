4ea04cb6db8009f79eaa2a91deb345b57ca42105
lrlna <shestak.irina@gmail.com>
2016-05-04 23:00:40 -0700

    position card based on whether sponsor is alum

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ var Trello = require('node-trello')
 var file = process.argv[2]
 
 var stream = csv({columns: true})
+
+// create a readable file stream
 var readable = fs.createReadStream(file)
   .pipe(stream)
-  .pipe(through2.obj(createCard))
+  .pipe(through2.obj(createTrello))
   .on('data', function (data) {
     console.log(data + "\n")
   })
@@ -23,22 +25,55 @@ var readable = fs.createReadStream(file)
   })
 
 
-function createCard (chunk, enc, callback) {
-  var boardId
+// ok, let's do some trello things;
+var idBoard, idList, boardURL, newCard
+
+function createTrello (chunk, enc, callback) {
+  // thing we need to create a list, and then cards
 
   var creds = getCreds()
   var trello = new Trello(creds.key, creds.token)
 
-  trello.post('/1/boards', {name: 'CascadiaFest Sponsors'}, function (err, data) {
-    if (err) throw (err)
+  // let's create a new board
+  //trello.post('/1/boards', {name: 'test12'}, function (err, data) {
+  //  if (err) throw err
+  //  idBoard = data.id
+  //  boardURL = data.url
+  //})
 
-    console.log(data)
+  // let's create a new list
+  //trello.post('/1/lists', {name: 'test213', idBoard: idBoard}, function (err, data) {
+  //  if (err) throw err
+  //  idList = data.id
+  //})
+
+  // let's create some cards based on 
+  Object.keys.forEach(chunk, function (keys) {
+    newCard = {
+      name: chunk['Company'],
+      //idList: idList,
+      desc: [
+        `**key**   ${chunk['Company']}`,
+        `**`
+      ].join("\b")
+      pos: checkIfAlum(chunk['Alum?'])
+    }
   })
+
+  //trello.post('/1/cards', newCard, function (err, data) {
+
+  })
+  
 }
 
 function getCreds () {
   var credsFile = path.join(__dirname, "./keys.json")
-  var file = fs.readFileSync(credsFile) 
+  var file = fs.readFileSync(credsFile)
 
   return JSON.parse(file)
+}
+
+function checkIfAlum (alum) {
+  var position = alum === "Y" ? 'top' : 'bottom'
+  return position
 }
